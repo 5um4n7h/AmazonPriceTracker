325008773d26c6211ed986beccae5d34c3ccad1f
Sumanth Hegde <sumanthhegde13@gmail.com>
2023-08-20 22:17:40 +0530

    revised

--- src/main/java/com/sumanth/amazonpricetracker/AmazonPriceTrackerApplication.java
+++ src/main/java/com/sumanth/amazonpricetracker/AmazonPriceTrackerApplication.java
@@ -1,5 +1,10 @@
 package com.sumanth.amazonpricetracker;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -11,33 +16,37 @@ public class AmazonPriceTrackerApplication {
 	public static void main(String[] args) {
 
 		SpringApplication.run(AmazonPriceTrackerApplication.class, args);
-		String productUrl = "https://www.amazon.in/OnePlus-Nord-Pastel-128GB-Storage/dp/B0BY8JZ22K/ref=sr_1_1?keywords=B0BY8JZ22K&qid=1692543631&sr=8-1";
-
-//		try {
-//			Document document = Jsoup.connect(productUrl).get();
+//		String productUrl = "https://www.amazon.in/OnePlus-Nord-Pastel-128GB-Storage/dp/B0BY8JZ22K/ref=sr_1_1?keywords=B0BY8JZ22K&qid=1692543631&sr=8-1";
 //
-//			// Find the element that contains the price information
-//			// Use the class name "a-price-whole" to target the price
-//			Element priceElement = document.select("span.a-price > span.a-offscreen").first();
-//			System.out.println(priceElement);
+//		System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver.exe");
+//
+//		ChromeOptions options = new ChromeOptions();
+//		options.addArguments("--headless"); // Run in headless mode (no GUI)
+//
+//		WebDriver driver = new ChromeDriver(options);
+//
+//		try {
+//			driver.get(productUrl);
+//
+//			// Find the element that contains the price information using the span class
+//			WebElement priceElement = driver.findElement(By.className("a-offscreen"));
 //
 //			if (priceElement != null) {
-//				String productPrice = priceElement.text();
+//				String productPrice = priceElement.getText();
 //				System.out.println("Product Price: " + productPrice);
 //			} else {
 //				System.out.println("Price not found.");
 //			}
-//		} catch (IOException e) {
-//			e.printStackTrace();
-//		}
 //
-//		try {
-//			Document document = Jsoup.connect(productUrl).get();
-//			String pageContent = document.outerHtml(); // Get the entire HTML content
-//			System.out.println(pageContent);
-//		} catch (IOException e) {
+//			WebElement divElement = driver.findElement(By.id("corePriceDisplay_desktop_feature_div"));
+//
+//			// Now you can work with the div element
+//			System.out.println("Div Content: " + divElement.getText());
+//		} catch (Exception e) {
 //			e.printStackTrace();
-//		}
+//		}  finally {
+//		driver.quit();
+//	}
 
 
 	}
